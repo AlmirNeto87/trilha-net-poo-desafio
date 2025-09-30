dc77c62e790299655330558704dc7e7df56297ed
Antonio Almir <149828632+AlmirNeto87@users.noreply.github.com>
2025-09-30 02:08:57 -0300

    Instanciando claasses segundo o exemplo

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
-Iphone iphone = new Iphone("12345", "iPhone 14", "IMEI123456", 128);
-Nokia nokia = new Nokia("98745635", "Nokia 3310", "IMEI12456", 64);
+Smartphone iphone = new Iphone("12345", "iPhone 14", "IMEI123456", 128);
+Smartphone nokia = new Nokia("98745635", "Nokia 3310", "IMEI12456", 64);
 
 nokia.Ligar();
 iphone.Ligar();
